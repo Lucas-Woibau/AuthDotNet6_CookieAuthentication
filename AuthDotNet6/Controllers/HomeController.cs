f18e7cfd27e6c2fc2e4ba54e50dd6748ab553b0a
Lucas Woibau <lucaswoibau@hotmail.com>
2023-01-04 14:31:40 -0300

    Add project files.

---
+++ AuthDotNet6/Controllers/HomeController.cs
@@ -0,0 +1,64 @@
+﻿using AuthDotNet6.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using System.Security.Claims;
+
+namespace AuthDotNet6.Controllers
+{
+    public class HomeController : Controller
+    {
+        [AllowAnonymous]
+        public IActionResult Index(bool erroLogin)
+        {
+            if (erroLogin)
+            {
+                ViewBag.Erro = "UserName or Password is incorrect!";
+            }
+            if (HttpContext.User.Identity.IsAuthenticated)
+            {
+                return RedirectToAction("Profile");
+            }
+            return View();
+        }
+
+        [HttpPost, AllowAnonymous]
+        public async Task<IActionResult> Login(User user)
+        {
+            var userDB = new User()
+            {
+                UserName = "Lucas",
+                Password = "123",
+                UserOffice = ""
+            };
+
+            if (!userDB.UserName.Equals(user.UserName) ||
+                !userDB.Password.Equals(user.Password)
+                )
+            {
+                return RedirectToAction("Index", new { erroLogin = true });
+            }
+
+            //Creating user's cookie
+            await new Services().Login(HttpContext, user);
+            return RedirectToAction("Profile");
+
+        }
+
+        //To protect from the other peoples not authorized
+        [Authorize]
+        public async Task<IActionResult> Out()
+        {
+            await new Services().Logoff(HttpContext);
+            return RedirectToAction("Index");
+        }
+
+        //Needs authorization
+        [Authorize]
+        public IActionResult Profile()
+        {
+            ViewBag.Permissions = HttpContext.User.Claims.Where(x => x.Type == ClaimTypes.Role).Select(x => x.Value);
+            return View();
+        }
+    }
+}
---
+++ AuthDotNet6/Services.cs
@@ -0,0 +1,40 @@
+﻿using AuthDotNet6.Models;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using System.Security.Claims;
+
+namespace AuthDotNet6
+{
+    public class Services 
+    {
+        //Add user login function
+        public async Task Login(HttpContext context, User user)
+        {
+            var claims = new List<Claim>();
+            claims.Add(new Claim(ClaimTypes.Name, user.UserName));
+            claims.Add(new Claim(ClaimTypes.Role, user.UserOffice));
+
+            var claimsIdentity =
+                new ClaimsPrincipal(
+                    new ClaimsIdentity(
+                        claims, CookieAuthenticationDefaults.AuthenticationScheme)//Authentication with cookies with the claims than created up there
+                    ); 
+
+            //Cookie configurations
+            var authProperties = new AuthenticationProperties
+            {
+                ExpiresUtc = DateTime.Now.AddHours(8), //Cookie expiration time
+                IssuedUtc = DateTime.Now   //The date when the cookies has ben created
+            };
+
+            await context.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, claimsIdentity, authProperties);
+        }
+
+        //Add cookie remotion
+        public async Task Logoff(HttpContext contexto)
+        {
+            await contexto.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+        }
+
+    }
+}
